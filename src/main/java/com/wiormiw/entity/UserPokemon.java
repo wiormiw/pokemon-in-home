cbb2f2f064b9a343943c3aebcbae4d15ac387737
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-03-19 11:22:32 +0700

    fix: resolve hashcode/equals warning in entity

--- src/main/java/com/wiormiw/entity/UserPokemon.java
+++ src/main/java/com/wiormiw/entity/UserPokemon.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "user_pokemon")
 public class UserPokemon extends PanacheEntityBase {
@@ -15,4 +17,18 @@ public class UserPokemon extends PanacheEntityBase {
     @Id
     @Column(name = "pokemon_id")
     public Long pokemonId;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof UserPokemon that)) return false;
+
+        return Objects.equals(userId, that.userId) && Objects.equals(pokemonId, that.pokemonId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(userId);
+        result = 31 * result + Objects.hashCode(pokemonId);
+        return result;
+    }
 }
